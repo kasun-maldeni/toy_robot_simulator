9302ed9eb59af47e29ea3b01ef780397cdd3d733
Kasun Maldeni <kasun.maldeni@gmail.com>
2016-06-23 01:56:09 +0000

    Add tests and code for 'move' method for Robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,3 +1,21 @@
 class Robot
-    attr_accessor :x, :y, :f
+  attr_accessor :x, :y, :f
+  
+  def place(opts={})
+  	self.x = opts[:x]
+  	self.y = opts[:y]
+  	self.f = opts[:f]
+  end
+  
+  def move
+    if self.f == "north"
+    	self.y += 1
+    elsif self.f == "east"
+    	self.x += 1
+    elsif self.f == "south"
+    	self.y -= 1
+    elsif self.f == "west"
+    	self.x -= 1
+    end
+  end
 end
--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -2,19 +2,42 @@ require 'spec_helper'
 require 'robot'
 
 describe Robot do
-    subject(:robot) { Robot.new }
-    
-    context "before a valid 'place' command is executed" do
-        it 'should not have an x co-ordinate' do
-            robot.x.should be_nil
-        end
-        
-        it 'should not have a y co-ordinate' do
-            robot.y.should be_nil
-        end
-        
-        it 'should not have a facing direction' do
-            robot.f.should be_nil
-        end
+  subject(:robot) { Robot.new }
+  
+  context "before a valid 'place' command is executed" do
+    it 'should not have an x co-ordinate' do
+      robot.x.should be_nil
     end
+    
+    it 'should not have a y co-ordinate' do
+      robot.y.should be_nil
+    end
+    
+    it 'should not have a facing direction' do
+      robot.f.should be_nil
+    end
+  end
+  
+  context "after a valid 'place' command is executed" do
+    before { robot.place(x: 2, y: 3, f: "north") }
+    
+    it "should increase the y co-ordinate when facing 'north' by using the 'move' command" do
+      expect { robot.move }.to change { robot.y }.by(1)
+    end
+    
+    it "should increase the x co-ordinate when facing 'east' by using the 'move' command" do
+      robot.f = "east"
+      expect { robot.move }.to change { robot.x }.by(1)
+    end
+    
+    it "should decrease the y co-ordinate when facing 'south' by using the 'move' command" do
+      robot.f = "south"
+      expect { robot.move }.to change { robot.y }.by(-1)
+    end
+    
+    it "should decrease the x co-ordinate when facing 'west' by using the 'move' command" do
+      robot.f = "west"
+      expect { robot.move }.to change { robot.x }.by(-1)
+    end
+  end
 end
