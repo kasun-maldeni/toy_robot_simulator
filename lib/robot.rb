6c528336b7c87b64029e39dfb122c61e8ca4e718
Kasun Maldeni <kasun.maldeni@gmail.com>
2016-06-23 03:39:28 +0000

    Add tests and code for robot to be able to turn and report its location

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,13 +1,16 @@
 class Robot
   attr_accessor :x, :y, :f
   
+  FACING = ["north", "east", "south", "west"]
   X_RANGE = 0..5
   Y_RANGE = 0..5
   
   def place(opts={})
-  	self.x = opts[:x]
-  	self.y = opts[:y]
-  	self.f = opts[:f]
+    if valid_place_command? opts
+    	self.x = opts[:x]
+    	self.y = opts[:y]
+    	self.f = opts[:f]
+    end
   end
   
   def move
@@ -20,5 +23,42 @@ class Robot
     elsif self.f == "west" && self.x > X_RANGE.min
     	self.x -= 1
     end
+  end
+  
+  def left
+    turn { |f| f == 0 ? 3 : f - 1 }
+  end
+  
+  def right
+    turn { |f| f == 3 ? 0 : f + 1 }
+  end
+  
+  def report
+    puts self
+  end
+  
+  private
+  def to_s
+    "Output: #{self.x},#{self.y},#{self.f.upcase}\n"
+  end
+  
+  def turn
+    f_val = FACING.index(self.f)
+    new_f_val = yield f_val
+    self.f = FACING[new_f_val]
+  end
+  
+  def valid_place_command?(opts={})
+    verify_coordinate?(opts[:x], X_RANGE) &&
+    verify_coordinate?(opts[:y], Y_RANGE) &&
+    verify_facing?(opts[:f])
+  end
+  
+  def verify_coordinate?(coordinate, range)
+    coordinate.is_a?(Integer) && range.include?(coordinate)
+  end
+  
+  def verify_facing?(f)
+    FACING.include?(f)
   end
 end
--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -18,45 +18,112 @@ describe Robot do
     end
   end
   
+  context "when trying to execute a valid 'place' command" do
+    it "should not accept an x co-ordinate outside the range of the grid" do
+      expect { robot.place(x: 6, y: 1, f: "north") }.to_not change { robot.x }
+    end
+    
+    it "should not accept a y co-ordinate outside the range of the grid" do
+      expect { robot.place(x: 2, y: 6, f: "north") }.to_not change { robot.y }
+    end
+    
+    it "should not accept a co-ordinate that is not an integer" do
+      expect { robot.place(x: "a", y: 2, f: "north") }.to_not change { robot.x }
+    end
+    
+    it "should not accept a direction outside of north, south, east or west" do
+      expect { robot.place(x: 1, y: 1, f: "random") }.to_not change { robot.f }
+    end
+  end
+  
   context "after a valid 'place' command is executed" do
     before { robot.place(x: 2, y: 3, f: "north") }
     
-    it "should increase the y co-ordinate when facing 'north' by using the 'move' command" do
+    # Left Turn
+    it "should change its facing direction from north to west when turning 'left'" do
+      robot.f = "north"
+      expect { robot.left }.to change { robot.f }.from("north").to("west")
+    end
+    
+    it "should change its facing direction from west to south when turning 'left'" do
+      robot.f = "west"
+      expect { robot.left }.to change { robot.f }.from("west").to("south")
+    end
+    
+    it "should change its facing direction from south to east when turning 'left'" do
+      robot.f = "south"
+      expect { robot.left }.to change { robot.f }.from("south").to("east")
+    end
+    
+    it "should change its facing direction from east to north when turning 'left'" do
+      robot.f = "east"
+      expect { robot.left }.to change { robot.f }.from("east").to("north")
+    end
+    
+    # Right Turn
+    it "should change its facing direction from north to east when turning 'right'" do
+      robot.f = "north"
+      expect { robot.right }.to change { robot.f }.from("north").to("east")
+    end
+    
+    it "should change its facing direction from east to south when turning 'right'" do
+      robot.f = "east"
+      expect { robot.right }.to change { robot.f }.from("east").to("south")
+    end
+    
+    it "should change its facing direction from south to west when turning 'right'" do
+      robot.f = "south"
+      expect { robot.right }.to change { robot.f }.from("south").to("west")
+    end
+    
+    it "should change its facing direction from west to north when turning 'right'" do
+      robot.f = "west"
+      expect { robot.right }.to change { robot.f }.from("west").to("north")
+    end
+    
+    # Move
+    it "should be moving north when facing 'north' and using the 'move' command" do
       expect { robot.move }.to change { robot.y }.by(1)
     end
     
-    it "should increase the x co-ordinate when facing 'east' by using the 'move' command" do
+    it "should be moving east when facing 'east' and using the 'move' command" do
       robot.f = "east"
       expect { robot.move }.to change { robot.x }.by(1)
     end
     
-    it "should decrease the y co-ordinate when facing 'south' by using the 'move' command" do
+    it "should be moving south when facing 'south' and using the 'move' command" do
       robot.f = "south"
       expect { robot.move }.to change { robot.y }.by(-1)
     end
     
-    it "should decrease the x co-ordinate when facing 'west' by using the 'move' command" do
+    it "should be moving west when facing 'west' and using the 'move' command" do
       robot.f = "west"
       expect { robot.move }.to change { robot.x }.by(-1)
     end
     
+    # Report
+    it "should be outputting its position when executing 'report'" do
+      $stdout.should_receive(:write)
+      robot.report
+    end
+    
     context "when robot is on the edge of the grid" do
-      it "should not be able to 'move' north when its y co-ordinate is at its max" do
+      it "should not be able to 'move' north when its at the northern most point in the grid" do
         robot.place(x: 2, y: 5, f: "north")
         expect { robot.move }.to_not change { robot.y }
       end
       
-      it "should not be able to 'move' east when its x co-ordinate is at its max" do
+      it "should not be able to 'move' east when its at the eastern most point in the grid" do
         robot.place(x: 5, y: 3, f: "east")
         expect { robot.move }.to_not change { robot.x }
       end
       
-      it "should not be able to 'move' south when its y co-ordinate is at its min" do
+      it "should not be able to 'move' south when its at the southern most point in the grid" do
         robot.place(x: 3, y: 0, f: "south")
         expect { robot.move }.to_not change { robot.y }
       end
       
-      it "should not be able to 'move' west when its x co-ordinate is at its min" do
+      it "should not be able to 'move' west when its at the western most point in the grid" do
         robot.place(x: 0, y: 0, f: "west")
         expect { robot.move }.to_not change { robot.x }
       end
